132cc4efdec472783583911b48c4ebed9101bb04
jafarhussain <jafarhussain@users.noreply.github.com>
2023-02-06 18:38:39 +0530

    basic_function_commit

---
+++ function.rs
@@ -0,0 +1,23 @@
+//  if the function returns a value, the return type must be specified after an arrow ->
+
+
+fn main()
+
+{
+
+show_to(99);
+    
+}
+
+fn show_to(n: u64) 
+{
+
+    for n in 1..=n
+    
+    
+    {
+
+        println!("{}th Number is : {}", n, n);
+
+    }
+}
